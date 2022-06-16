cb9b4ebebd00daca512fd2436ea76db5a701a448
Ke Li <keli2023@bu.edu>
2022-06-16 10:43:04 -0400

    Adding more feature for product screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-function ProductScreen() {
-  return (
-    <div>ProductScreen</div>
-  )
+function inStock( s ) {
+  if (s> 0){
+    return (
+      <p className="text-success">In Stock</p>
+    );
+  }else{
+    return(
+      <p className="text-danger">Out of Stock</p>
+    );
+  }
+
 }
 
-export default ProductScreen
+function ProductScreen() {
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
+
+  return (
+    <div>
+      <Link to="/" className="btn btn-secondary my-3">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item >
+              <h4>{product.name}</h4>
+            </ListGroup.Item>
+            <ListGroup.Item >
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h5>Price: ${product.price}</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Col>Price: </Col>
+                <Col>
+                <strong>${product.price}</strong>
+                </Col>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Col>Status: </Col>
+                <Col>
+                 {inStock(product.countInStock)}
+                </Col>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductScreen;
